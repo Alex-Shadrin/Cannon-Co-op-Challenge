30247b7c1c3138db383d47f48ec397cd30f8696c
Alex Shadrin <alex.shad111@gmail.com>
2023-06-24 00:30:27 +0400

    add fix laser & explosion effect

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,13 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private GameObject _explosion;
+    [SerializeField] private GameObject _smoke;
     public int health;
     public int maxHelth = 100;
-
+    bool once;
     private void Start()
     {
         health = maxHelth;
@@ -19,8 +20,20 @@ public class Health : MonoBehaviour
         if (health <= 0)
         {
             health = 0;
-            SceneManager.LoadScene("SampleScene");
+            StartCoroutine(GameOver());
         }
+    }
+
+    public IEnumerator GameOver()
+    {
+        if (!once)
+        {
+            Instantiate(_explosion, transform.position, Quaternion.identity);
+            Instantiate(_smoke, transform.position, Quaternion.identity);
+            once = true;
+        }
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void SetHealth(int bonusHealth)
--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -4,47 +4,48 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-    public float Length;
-    public LayerMask LayerMask;
+    [SerializeField] private float Length;
+    [SerializeField] private LineRenderer _lineRenderer;
+    [SerializeField] private Transform _ray;
 
-    private LineRenderer _lineRenderer;
+    [SerializeField] private ParticleSystem _muzzleParticles;
+    [SerializeField] private ParticleSystem _hitParticles;
+
+    public LayerMask LayerMask;
 
     private void Start()
     {
         _lineRenderer = GetComponent<LineRenderer>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        //_lineRenderer.SetPosition(0, transform.position);
         Ray ray = new Ray(transform.position, -Vector3.up);
 
         //Debug.DrawRay(transform.position, ray.direction * Length, Color.green);
         //_lineRenderer.SetPosition(1, ray.direction * Length );
 
-        if (Physics.Raycast(ray,out var hit, Length, LayerMask))
+        //if (Physics.Raycast(ray,out var hit, Length, LayerMask))
+        //{
+        //    Debug.Log("Hit");
+
+        //    if (hit.transform.gameObject.TryGetComponent<Health>(out var health))
+        //    {
+        //        health.TakeHit(10);
+        //    }
+        //}
+
+        bool cast = Physics.Raycast(ray, out var hit, Length, LayerMask);
+
+        Vector3 hitPosition = cast ? hit.point : _ray.position + -Vector3.up * Length;
+        _lineRenderer.SetPosition(0, _ray.position);
+        _lineRenderer.SetPosition(1, hitPosition);
+
+        if (cast && hit.collider.gameObject.TryGetComponent<Health>(out var health))
         {
-            Debug.Log("Hit");
-            //_lineRenderer.SetPosition(1, hit.point);
-            if (hit.transform.gameObject.TryGetComponent<Health>(out var health))
-            {
-                health.TakeHit(10);
-            }
+            health.TakeHit(1);
         }
+
+        _hitParticles.transform.position = hitPosition;
     }
-
-
-    //private void Update()
-    //{
-    //    _lineRenderer.SetPosition(0, transform.position);
-    //    RaycastHit hit;
-    //    if (Physics.Raycast(transform.position, -transform.up, out hit))
-    //    {
-    //        if (hit.collider)
-    //        {
-    //            _lineRenderer.SetPosition(1, hit.point);
-    //        }
-    //    }
-    //    else _lineRenderer.SetPosition(1, -transform.up * 10);
-    //}
 }
